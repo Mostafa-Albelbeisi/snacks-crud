8d5724712d3436e59ceae71e27d588542f94f15d
Mostafa-Albelbeisi <mostfabelbeisi@gmail.com>
2022-12-05 11:41:15 +0000

    v10

--- snacks/urls.py
+++ snacks/urls.py
@@ -3,9 +3,9 @@ from .views import SnackList, SnackDetail, SnackUpdate, SnackCreate, SnackDelete
 
 urlpatterns = [
     # path('', HomePage.as_view(), name='home'),
-    path('', SnackList.as_view(), name="SnackListView"),
-    path('<int:pk>/', SnackDetail.as_view(), name='SnackDetailView'),
-    path('create/', SnackCreate.as_view(), name='SnackCreateView'),
-    path('<int:pk>/update/', SnackUpdate.as_view(), name='SnackUpdateView'),
-    path('<int:pk>/delete/', SnackDelete.as_view(), name='SnackDeleteView'),
+    path("", SnackList.as_view(), name="SnackListView"),
+    path("<int:pk>/", SnackDetail.as_view(), name="SnackDetailView"),
+    path("create/", SnackCreate.as_view(), name="SnackCreateView"),
+    path("<int:pk>/update/", SnackUpdate.as_view(), name="SnackUpdateView"),
+    path("<int:pk>/delete/", SnackDelete.as_view(), name="SnackDeleteView"),
 ]
